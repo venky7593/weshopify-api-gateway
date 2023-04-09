331070da8ab6f1c377235b2694f7f75b6495d7de
Narsi <nsanda@innominds.com>
2023-04-09 19:15:47 +0530

    added the weshopify-api-gateway

---
+++ weshopify-platform-api-gateway/src/main/java/com/weshopify/platform/WeshopifyPlatformApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.weshopify.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class WeshopifyPlatformApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeshopifyPlatformApiGatewayApplication.class, args);
+	}
+	
+
+	
+}
